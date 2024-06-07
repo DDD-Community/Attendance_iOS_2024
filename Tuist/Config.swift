501b1b7fc0fd7c8483f2ebbffe6daea1f80dde39
Roy <shuwj81@daum.net>
2024-06-08 00:54:20 +0900

    ✨[feat]: Tuist 모듈 화 셋업

--- Tuist/Config.swift
+++ Tuist/Config.swift
@@ -6,3 +6,15 @@
 //
 
 import Foundation
+import ProjectDescription
+
+let config = Config(
+//    compatibleXcodeVersions: ["15.2"],
+    swiftVersion: "5.10.0",
+    plugins: [
+        .local(path: .relativeToRoot("Plugins/ProjectTemplatePlugin")),
+        .local(path: .relativeToRoot("Plugins/DependencyPackagePlugin")),
+        .local(path: .relativeToRoot("Plugins/DependencyPlugin")),
+    ],
+    generationOptions: .options()
+)
--- WorkSpace.swift
+++ WorkSpace.swift
@@ -6,3 +6,12 @@
 //
 
 import Foundation
+import ProjectDescription
+import ProjectTemplatePlugin
+
+
+let workspace = Workspace(
+name: Project.Environment.appName,
+projects: [
+    "Projects/**"
+])
