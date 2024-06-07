91e925e7d313011ca78d39e226669396f34017f6
Roy <shuwj81@daum.net>
2024-06-08 00:54:01 +0900

    ✨[feat]: Tuist 모듈 화 셋업

---
+++ Plugins/DependencyPackagePlugin/ProjectDescriptionHelpers/DependencyPackage/Extension+TargetDependency.swift
@@ -0,0 +1,16 @@
+//
+//  Extension+TargetDependency.swift
+//  Plugins
+//
+//  Created by 서원지 on 2/21/24.
+//
+
+import ProjectDescription
+
+public extension TargetDependency {
+    enum SPM {}
+    
+}
+
+
+
---
+++ Plugins/DependencyPackagePlugin/ProjectDescriptionHelpers/DependencyPackage/Extension+TargetDependencySPM.swift
@@ -0,0 +1,33 @@
+//
+//  Extension+TargetDependencySPM.swift
+//  DependencyPackagePlugin
+//
+//  Created by 서원지 on 4/19/24.
+//
+
+import ProjectDescription
+
+public extension TargetDependency.SPM {
+    static let moya = TargetDependency.external(name: "Moya", condition: .none)
+    static let combineMoya = TargetDependency.external(name: "CombineMoya", condition: .none)
+    static let composableArchitecture = TargetDependency.external(name: "ComposableArchitecture", condition: .none)
+    static let then = TargetDependency.external(name: "Then", condition: .none)
+    static let flexLayout = TargetDependency.external(name: "FlexLayout", condition: .none)
+    static let pinLayout = TargetDependency.external(name: "PinLayout", condition: .none)
+    static let reactorKit = TargetDependency.external(name: "ReactorKit", condition: .none)
+    static let rxCocoa = TargetDependency.external(name: "RxCocoa", condition: .none)
+    static let snapKit = TargetDependency.external(name: "SnapKit", condition: .none)
+    static let qrCodeSwift = TargetDependency.external(name: "QRCodeSwift", condition: .none)
+    static let googleSignIn = TargetDependency.external(name: "GoogleSignIn", condition: .none)
+    static let firebaseAuth = TargetDependency.external(name: "FirebaseAuth", condition: .none)
+    static let firebaseFirestore = TargetDependency.external(name: "FirebaseFirestore", condition: .none)
+    static let firebaseAnalytics = TargetDependency.external(name: "FirebaseAnalytics", condition: .none)
+    static let firebaseCrashlytics = TargetDependency.external(name: "FirebaseCrashlytics", condition: .none)
+    static let firebaseRemoteConfig = TargetDependency.external(name: "FirebaseRemoteConfig", condition: .none)
+    
+    
+    
+
+}
+  
+
