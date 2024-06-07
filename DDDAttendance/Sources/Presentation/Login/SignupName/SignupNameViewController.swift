102b55b2acb9d427d1a13f03e79361d85327bc22
Roy <shuwj81@daum.net>
2024-06-08 00:53:54 +0900

    ✨[feat]: Tuist 모듈 화 셋업

--- DDDAttendance/Sources/Presentation/Login/SignupName/SignupNameViewController.swift
+++
@@ -1,8 +0,0 @@
-//
-//  SignupNameViewController.swift
-//  DDDAttendance
-//
-//  Created by 고병학 on 6/6/24.
-//
-
-import Foundation
--- DDDAttendance/Sources/Presentation/Login/SignupPart/SignupPartViewController.swift
+++
@@ -1,8 +0,0 @@
-//
-//  SignupPartViewController.swift
-//  DDDAttendance
-//
-//  Created by 고병학 on 6/6/24.
-//
-
-import Foundation
