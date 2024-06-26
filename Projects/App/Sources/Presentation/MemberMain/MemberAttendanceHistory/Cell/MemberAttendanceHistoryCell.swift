2e36a06db682fa0667b8d883327a7bf06cf4e22b
bengHak <qkwl4678@naver.com>
2024-06-27 02:52:48 +0900

    feat: 멤버 출석 기록

---
+++ Projects/App/Sources/Presentation/MemberMain/MemberAttendanceHistory/Cell/MemberAttendanceHistoryCell.swift
@@ -0,0 +1,25 @@
+//
+//  MemberAttendanceHistoryCell.swift
+//  DDDAttendance
+//
+//  Created by 고병학 on 6/27/24.
+//
+
+import FlexLayout
+import PinLayout
+import Then
+
+import UIKit
+
+final class MemberAttendanceHistoryCell: UICollectionViewCell {
+    // MARK: - UI properties
+    
+    // MARK: - Properties
+    static let identifier: String = "\(MemberAttendanceHistoryCell.self)"
+    
+    // MARK: - Lifecycles
+    
+    // MARK: - Public helpers
+    
+    // MARK: - Private helpers
+}
